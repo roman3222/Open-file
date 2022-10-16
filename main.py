1f33095723ad767115de335115b509e910d79044
RomanKhopkin <roman.khopkin3222@gmail.com>
2022-10-16 23:40:30 +0900

    change main

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
+def food_book():
+    with open('book.txt', 'r', encoding = 'utf8') as cook_book:
+        global book
+        book = {}
+        for line in cook_book:
+            dish_name = line
+            book = {dish_name: []}
+            ingredient_count = cook_book.readline()
+            for i in range(int(ingredient_count)):
+                emp = cook_book.readline()
+                ingredient, quantity, measure = emp.split('|')
+                dinner = {'ingredient': ingredient, 'quantity': int(quantity), 'measure': measure}
+                book[dish_name].append(dinner)
+            cook_book.readline()
+            print(book)
+    return
+
+def get_shop_list_by_dishes(dishes, person_count):
+    for i in book:
+        print(i['Омлет'])
 
 
-book = {}
-with open('book.txt', 'r', encoding = 'utf8') as cook_book:
-    for line in cook_book:
-        dish_name = line
-        book = {dish_name: []}
-        ingredient_count = cook_book.readline()
-        for i in range(int(ingredient_count)):
-            emp = cook_book.readline()
-            ingredient, quantity, measure = emp.split('|')
-            dinner = {'ingredient': ingredient, 'quantity': int(quantity), 'measure': measure}
-            book[dish_name].append(dinner)
-        cook_book.readline()
 
 
 
-    def get_shop_list_by_dishes(dishes, person_count):
-        for i in book:
-            print(i['Омлет'])
+
 
 
 
