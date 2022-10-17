24dd00750fc8264f2f4dd245ca1f5557850e680a
RomanKhopkin <roman.khopkin3222@gmail.com>
2022-10-17 22:03:30 +0900

    change func

--- main.py
+++ main.py
@@ -17,9 +17,23 @@ def food_book():
 food_book()
 
 def get_shop_list_by_dishes(dishes, person_count):
-    for i in book:
-        if dishes in i:
-            
+    for dish in book:
+        return dish
+
+
+
+
+print(get_shop_list_by_dishes(['Утка по-пекински'], 2))
+
+
+
+dct = {1:[1, 2, 3, 4], 4: [6, 7, 8]}
+res = list(dct)
+print(res)
+
+
+
+
 
 
 
